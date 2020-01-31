283aa46372092f23325db0ddfbd45898e8a015e5
liboyan <liboyan_lby@126.com>
2020-01-31 16:45:54 +0800

    springboot整合异步任务以及使用场景

--- src/main/java/com/imooc/controller/ThymeleafController.java
+++ src/main/java/com/imooc/controller/ThymeleafController.java
@@ -3,7 +3,12 @@ package com.imooc.controller;
 import com.imooc.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @Description:
@@ -23,9 +28,52 @@ public class ThymeleafController {
         return "thymeleaf/center/center";
     }
 
+    @RequestMapping("test")
     public String test(ModelMap map){
-        User user = new User();
-        return null;
+        User u = new User();
+        u.setName("superadmin");
+        u.setAge(10);
+        u.setPassword("123465");
+        u.setBirthday(new Date());
+        u.setDesc("<font color='green'><b>hello imooc</b></font>");
+
+        map.addAttribute("user", u);
+
+        User u1 = new User();
+        u1.setAge(19);
+        u1.setName("imooc");
+        u1.setPassword("123456");
+        u1.setBirthday(new Date());
+
+        User u2 = new User();
+        u2.setAge(17);
+        u2.setName("LeeCX");
+        u2.setPassword("123456");
+        u2.setBirthday(new Date());
+
+        List<User> userList = new ArrayList<>();
+        userList.add(u);
+        userList.add(u1);
+        userList.add(u2);
+
+        map.addAttribute("userList", userList);
+
+        return "thymeleaf/test";
     }
+
+    @PostMapping("postform")
+    public String postform(User u){
+        System.out.println(u.getName());
+        return "redirect:/th/test";
+    }
+
+    @RequestMapping("showerror")
+    public String showerror(User u) {
+        int a = 1 / 0;
+        return "redirect:/th/test";
+    }
+
+
+
 
 }
---
+++ src/main/java/com/imooc/tasks/DoTask.java
@@ -0,0 +1,38 @@
+package com.imooc.tasks;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Future;
+
+/**
+ * @Description:
+ */
+@RestController
+@RequestMapping("/tasks")
+public class DoTask {
+
+    @Autowired
+    private AsyncTask asyncTask;
+
+    @RequestMapping("/test")
+    public String test1() throws Exception {
+        long start = System.currentTimeMillis();
+
+        Future<Boolean> task1 = asyncTask.doTask11();
+        Future<Boolean> task2 = asyncTask.doTask22();
+        Future<Boolean> task3 = asyncTask.doTask33();
+
+        while (!task1.isDone() || !task2.isDone() || !task3.isDone()) {
+            // 当所有的任务都执行完时，退出
+            if (task1.isDone() && task2.isDone() && task3.isDone()) {
+                break;
+            }
+        }
+        long end = System.currentTimeMillis();
+        String times = "任务全部完成，总耗时：" + (end - start) + "毫秒";
+        System.out.println(times);
+        return times;
+    }
+}
